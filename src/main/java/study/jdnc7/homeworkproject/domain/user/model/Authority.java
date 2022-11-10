f8393db6a175e4ee7dbc96b54b755bf2ddd01198
Kim Byung Jun <celbever@naver.com>
2022-11-10 23:48:28 +0900

    [Domain] 생성자 추가 및 기본 권한 제공 메소드 추가

--- src/main/java/study/jdnc7/homeworkproject/domain/user/model/Authority.java
+++ src/main/java/study/jdnc7/homeworkproject/domain/user/model/Authority.java
@@ -10,5 +10,16 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Authority {
+    private static final String ROLE_ADMIN= "ROLE_ADMIN";
+    private static final String ROLE_USER= "ROLE_USER";
+
     private String authorityName;
+
+    public static Authority getUserAuthority() {
+        return new Authority(ROLE_USER);
+    }
+
+    public static Authority getAdminAuthority() {
+        return new Authority(ROLE_ADMIN);
+    }
 }
--- src/main/java/study/jdnc7/homeworkproject/domain/user/model/User.java
+++ src/main/java/study/jdnc7/homeworkproject/domain/user/model/User.java
@@ -4,20 +4,34 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.ibatis.type.Alias;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Alias("User")
 public class User {
 
     private Long userId;
-    private String username;
+    private String userName;
     private String password;
-    private String nickname;
+    private String nickName;
     private boolean activated;
     private Set<Authority> authorities;
 
+    public static User toEntity(String userName, String password, String nickName, Authority authority) {
+        User user = new User();
+        user.setUserName(userName);
+        user.setPassword(password);
+        user.setNickName(nickName);
+        user.activated = true;
+        Set<Authority> set = new HashSet<>();
+        set.add(authority);
+        user.setAuthorities(set);
+        return user;
+    }
 }
