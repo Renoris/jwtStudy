7f7385de2cfac6fd55605c4879c20733c78bb48d
renoris <celbever@naver.com>
2022-11-12 01:28:13 +0900

    [Security] Security 등록

--- src/main/java/study/jdnc7/homeworkproject/config/SecurityConfig.java
+++ src/main/java/study/jdnc7/homeworkproject/config/SecurityConfig.java
@@ -1,36 +1,70 @@
 package study.jdnc7.homeworkproject.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import study.jdnc7.homeworkproject.jwt.JwtAccessDeniedHandler;
+import study.jdnc7.homeworkproject.jwt.JwtAuthenticationEntryPoint;
+import study.jdnc7.homeworkproject.jwt.TokenProvider;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //preAuthorize 를 메소드단위로 허용
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
-    public PasswordEncoder passwordEncoder () {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web
-                .ignoring().antMatchers("/h2-console/**","/favicon.ico");
+                .ignoring().antMatchers("/h2-console/**", "/favicon.ico");
     }
 
     @Bean
-    protected SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
+    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .csrf().disable() //토큰방식을 사용하기때문에 csrf 설정은 disable-  뭔소리?
+                .exceptionHandling()
+
+                //해당 부분을 우리가 만들었던 걸로 추가
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                //h2 콘솔을 위한 설정이라고함....
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                //우리는 세션을 사용하지 않기때문에 세션을 stateless로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                //로그인/회원가입은 토큰이 없는상태에서 진행되기때문에 허용
+                .and()
                 .authorizeRequests()
                 .antMatchers("/api/login").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+                .anyRequest().authenticated()
+
+                //마지막 우리가 만든 config 등록
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
 
         return httpSecurity.build();
     }
